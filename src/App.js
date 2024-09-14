c88aa10d122df8085e74d863ffe943d88ab3f6fa
abhishekshankar <abhishek.shankar@gmail.com>
2024-09-14 00:01:31 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import IVFCalendar from './IVFCalendar';
+
+function App() {
+  return (
+    <div className="App">
+      <IVFCalendar />
+    </div>
+  );
+}
+
+export default App;
